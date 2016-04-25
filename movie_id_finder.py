6a3202ea1058a8eb5fc8d84e67006139c7c8997d
arnavl105 <arnavl105@gmail.com>
2016-04-25 16:33:06 -0400

    limited movie id finder
    limited to just find 300 ids

--- movie_id_finder.py
+++ movie_id_finder.py
@@ -1,8 +1,12 @@
 import csv
 ids = set()
+count = 0
 with open("character_list5.csv") as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		ids.add(row["script_id"])
+		if count < 300:
+			ids.add(row["script_id"])
+		count = len(ids) 
+	print(len(ids)) 
 	f = open('ids.txt', 'w')
 	f.write(str(list(ids)))
